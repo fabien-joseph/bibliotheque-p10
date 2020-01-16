3782faa3de292ddff01b8c7f8a5550fe36a26271
Fabien Joseph <styleure@ascentia.fr>
2020-01-17 00:05:45 +0100

    Ajout des notifactions auto 5j avant l'expiration d'une reservation. Ticket #4

--- bibliotheque-webapp/src/main/java/com/bibliotheque/webapp/controller/ReservationController.java
+++ bibliotheque-webapp/src/main/java/com/bibliotheque/webapp/controller/ReservationController.java
@@ -8,16 +8,12 @@ import io.swagger.client.model.Livre;
 import io.swagger.client.model.Reservation;
 import io.swagger.client.model.Utilisateur;
 import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Calendar;
 import java.util.List;
